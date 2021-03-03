def53be1f763dcad87b9e14f6447113853c7d602
Volki312 <josip.volarevic4@gmail.com>
2021-03-03 21:32:40 +0100

    👷‍♂️ Code improvements and minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import useIsMounted from "./hooks/useIsMounted";
 import "./index.css";
 
 const sources: [string, string] = [
-  "https://youtu.be/J91ti_MpdHA",
-  "https://youtu.be/L_LUpnjgPso",
+  "http://media.w3.org/2010/05/sintel/trailer.mp4",
+  "http://media.w3.org/2010/05/bunny/movie.mp4",
 ];
 
 const App: React.FC = () => {
-  const [activeVideoIndex, setActiveVideoIndex] = useState<number>(0);
+  const [activeVideoIndex, setActiveVideoIndex] = useState<0 | 1>(0);
   const [areVideosPlaying, setAreVideosPlaying] = useState<boolean>(false);
   const isMounted = useIsMounted();
 
-  const toggleActiveVideo = () =>
+  const toggleActiveVideo = () => {
     setActiveVideoIndex((prevIndex) => (prevIndex === 1 ? 0 : 1));
+  };
+
+  const playVideos = () => setAreVideosPlaying(true);
+  const pauseVideos = () => setAreVideosPlaying(false);
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -33,39 +37,37 @@ const App: React.FC = () => {
   }, [isMounted]);
 
   return (
-    <main>
-      {/* TODO: Disable pause on video click and other controls */}
-      {sources.map((source, index) => (
-        <ReactPlayer
-          url={source}
-          key={index}
-          className="video"
-          playing={areVideosPlaying}
-          style={{ display: activeVideoIndex === index ? "block" : "none" }}
-          controls={false}
-          muted={activeVideoIndex !== index}
-          config={{
-            youtube: {
-              playerVars: {
-                controls: 0,
-                disablekb: 1,
-              },
-            },
-          }}
-        />
-      ))}
+    <div id="player">
+      <div className="video-wrapper" onClick={toggleActiveVideo}>
+        {sources.map((source, index) => (
+          <ReactPlayer
+            key={index}
+            url={source}
+            className="video"
+            style={{ display: activeVideoIndex === index ? "block" : "none" }}
+            width="100%"
+            height="100%"
+            playing={areVideosPlaying}
+            controls={false}
+            pip={false}
+            muted={activeVideoIndex !== index}
+            onBuffer={pauseVideos}
+            onBufferEnd={playVideos}
+          />
+        ))}
+      </div>
       <div className="controls">
-        <button className="control" onClick={() => setAreVideosPlaying(true)}>
+        <button className="control" onClick={playVideos}>
           &#9658;
         </button>
-        <button className="control" onClick={() => setAreVideosPlaying(false)}>
+        <button className="control" onClick={pauseVideos}>
           &#10074;&#10074;
         </button>
-        <button className="control" onClick={() => toggleActiveVideo()}>
+        <button className="control" onClick={toggleActiveVideo}>
           &#8633;
         </button>
       </div>
-    </main>
+    </div>
   );
 };
 
